320e8cbfb726406a9515ac0f0d129c5c625665b5
Dzmitry Chystsik <chystsik@gmail.com>
2023-12-20 02:22:38 +0300

    uncomment code

--- cmd/agent/env.go
+++ cmd/agent/env.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Chystik/runtime-metrics/config"
 
 	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
 )
 
 // parseEnv loads ENV from file if ENVIRONMENT val is set and parses ENV values
 func parseEnv(cfg *config.AgentConfig) error {
-	/* var envFile string
+	var envFile string
 	var err error
 
 	if osEnv := os.Getenv("ENVIRONMENT"); osEnv != "" {
@@ -25,7 +28,7 @@ func parseEnv(cfg *config.AgentConfig) error {
 		if err != nil {
 			panic(err)
 		}
-	} */
+	}
 
 	return env.Parse(cfg)
 }
